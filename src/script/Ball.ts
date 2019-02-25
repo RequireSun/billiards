828328680b82a279de27d354e909f9a34bb60bf5
kelvinsun <kelvinsun@tencent.com>
2019-02-25 11:21:21 +0800

    feat: 球的外框粗判断

--- src/script/Ball.ts
+++ src/script/Ball.ts
@@ -6,6 +6,14 @@ export default class Ball extends Laya.Script {
     /** @prop { name: number, tips:"球的号码，0 代表白球", type: int}*/
     number: number;
 
+    private _startX: number;
+    private _startY: number;
+    private _endX: number;
+    private _endY: number;
+
+    private _middleLeft: number;
+    private _middleRight: number;
+
     constructor() { 
         super(); 
     }
@@ -16,11 +24,6 @@ export default class Ball extends Laya.Script {
     //     rig.setVelocity({ x: 0, y: -10 });
     // }
 
-    onTriggerEnter(other: any, self: any, contact: any): void {
-        //如果被碰到，则移除子弹
-        // this.owner.removeSelf();
-    }
-
     onEnable(): void {
         if (this.number) {
             const label: Laya.Label = this.owner.getChildByName('label') as Laya.Label;
@@ -28,16 +31,58 @@ export default class Ball extends Laya.Script {
             label.text = `${this.number}`;
         }
 
+        const parent = this.owner.parent as Laya.Sprite;
+
+        this._startX = 22;//parent.x;
+        this._startY = 22;//parent.y;
+        this._endX = /*this._startX + */parent.width;
+        this._endY = /*this._startY + */parent.height;
+
+        this._middleLeft = parent.width / 2 - 22;
+        this._middleRight = parent.width / 2 + 22;
+
+        this._startX += 22;
+        this._startY += 22;
+        this._endX -= 22;
+        this._endY -= 22;
+
         Object.defineProperty(this.owner, 'startMove', {
             enumerable: false,
-            value() {
+            value(xForce: number, yForce: number): void {
                 var rig: Laya.RigidBody = this.getComponent(Laya.RigidBody);
-                const x: number = (.5 < Math.random() ? 1 : -1) * Math.random() * 10;
-                const y: number = (.5 < Math.random() ? 1 : -1) * Math.random() * 10;
 
-                rig.setVelocity({ x, y });
+                rig.setVelocity({ x: xForce, y: yForce });
             },
         });
+    }
+
+    onUpdate(): void {
+        const owner = this.owner as Laya.Sprite;
+
+        const { x, y } = owner;
+
+        if (x < this._startX) {
+            if (y < this._startY) {
+                console.log('左上洞');
+            } else if (y > this._endY) {
+                console.log('左下洞');
+            }
+        } else if (x > this._endX) {
+            if (y < this._startY) {
+                console.log('右上洞');
+            } else if (y > this._endY) {
+                console.log('右下洞');
+            }
+        } else if (y < this._startY) {
+            if (x > this._middleLeft && x < this._middleRight) {
+                console.log('中上洞');
+            }
+        } else if (y > this._endY) {
+            if (x > this._middleLeft && x < this._middleRight) {
+                console.log('中下洞');
+            }
+        }
+        // 其他情况就是在桌子中间了
     }
 
     // startMove(): void {
--- src/script/GameControl.ts
+++ src/script/GameControl.ts
@@ -18,9 +18,25 @@ export default class GameControl extends Laya.Script {
         //停止事件冒泡，提高性能，当然也可以不要
         e.stopPropagation();
 
-        let ball = this._gameBox.getChildByName('ball_white');
-        // ball = ball.getComponent(Ball);
-        (ball as any).startMove();
+        let ball = this._gameBox.getChildByName('ball_white') as Laya.Sprite;
+
+        const parent = ball.parent as Laya.Sprite;
+
+        const touchX = e.stageX;
+        const touchY = e.stageY;
+
+        const ballX = ball.x + parent.x;
+        const ballY = ball.y + parent.y;
+
+        const x: number = (touchX - ballX) / 20;
+        const y: number = (touchY - ballY) / 20;
+
+        console.log(touchX, touchY, ballX, ballY, x, y);
+
+        // const x: number = (.5 < Math.random() ? 1 : -1) * Math.random() * 10;
+        // const y: number = (.5 < Math.random() ? 1 : -1) * Math.random() * 10;
+
+        (ball as any).startMove(x, y);
         
         //舞台被点击后，使用对象池创建子弹
         // let flyer: Laya.Sprite = Laya.Pool.getItemByCreateFun("bullet", this.bullet.create, this.bullet);
